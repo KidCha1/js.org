84415726564f070bfb3597a4ad2977ebdcb5c176
KidCha1 <asterix@homi-ai.org>
2026-09-12 16:32:48 -0700

    Add kidcha.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1732,6 +1732,7 @@ var cnames_active = {
   "kloner": "fivefifteen.github.io/kloner",
   "kmeans": "jeff-tian.github.io/k-means",
   "knc": "chaituknag.github.io",
+  "kidcha": "kidcha1.github.io/kidcha",
   "ko": "ko25july.github.io/ko.js.org",
   "ko.mobx": "cname.vercel-dns.com", // noCF
   "ko.redux": "deminoth.github.io/redux", // noCF
